bc99eb26671a60c7ce412d2d8c695b94b26120e3
Osama Mansour <93058963+Eng-OsamaMansour@users.noreply.github.com>
2025-05-05 01:42:07 +0300

    Testing and fixing errors for Clinic Create API

--- PalClinic/Clinic/models.py
+++ PalClinic/Clinic/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.gis.db import models as geomodels
 class Clinic(models.Model):
     class ClinicType(models.TextChoices):
-        INDV = 'indvisual'
+        INDV = 'individual'
         HCCR = 'healthcarecenter'
     
     name = models.CharField(max_length=50, unique=True,default="UNDEFINED")
@@ -11,7 +11,7 @@ class Clinic(models.Model):
     phoneNumber = models.CharField(max_length=15,default="UNDEFINED")
     email = models.EmailField(max_length=100,default="UNDEFINED@UNDEFINED",unique=True)
     location = geomodels.PointField(srid=4326, geography=True, null=True, blank=True)
-    specialties	= models.CharField(max_length=100,default="UNDEFINED")
+    specialties	= models.TextField(max_length=100,default="UNDEFINED")
     operating_hours = models.JSONField(default=dict,help_text="Structured hours, e.g. {'mon': '9-5', 'tue': '10-4'}",blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
--- PalClinic/Clinic/serializers.py
+++ PalClinic/Clinic/serializers.py
@@ -3,9 +3,11 @@ from .models import Clinic
 
 class ClinicSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Clinic
         fields = ['name','clinictype','address','phoneNumber','email','location','specialties','operating_hours']
         read_only_fields = ['created_at']
     def validate(self, attrs):
         data = self.context['request'].data
         if Clinic.objects.filter(name = data.get('name')).exists():
             raise ValueError({'detail':'this clinic is alredy exisits'})
+        return attrs
