ba8406b0eba19338091a17d13b9eb5871b06123e
Osama Mansour <93058963+Eng-OsamaMansour@users.noreply.github.com>
2025-05-05 01:27:57 +0300

    Create clinic API + serlizer

---
+++ PalClinic/Clinic/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Clinic
+
+class ClinicSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ['name','clinictype','address','phoneNumber','email','location','specialties','operating_hours']
+        read_only_fields = ['created_at']
+    def validate(self, attrs):
+        data = self.context['request'].data
+        if Clinic.objects.filter(name = data.get('name')).exists():
+            raise ValueError({'detail':'this clinic is alredy exisits'})
--- PalClinic/Clinic/views.py
+++ PalClinic/Clinic/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics,permissions
+from AccessControl.permissions import *
+from .serializers import *
 
-# Create your views here.
+class ClinicCreateView(generics.CreateAPIView):
+    serializer_class = ClinicSerializer
+    http_method_names = ['post']
+    permission_classes = [permissions.IsAuthenticated,IsAdmin]
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
