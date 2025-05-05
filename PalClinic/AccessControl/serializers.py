19dc7165f34ca3d633544eb7df4ca829d2ff277b
Osama Mansour <93058963+Eng-OsamaMansour@users.noreply.github.com>
2025-05-05 04:16:28 +0300

    Assign doctor to clinic serializer

--- PalClinic/AccessControl/serializers.py
+++ PalClinic/AccessControl/serializers.py
@@ -59,4 +59,20 @@ class AssignClinicToHealthCenterSerializer(serializers.ModelSerializer):
             raise ValidationError("The Clinic Does Not exist")
         if not Clinic.objects.filter(id = health).exists():
             raise ValidationError("The HealthCenter Does Not exists")
+        if AssignClinicToHealthCenter.objects.filter(clinic = clinic, is_acive=True).exists():
+            raise ValidationError("The Clinic is Already Assigned to health care center")
         return attrs
+
+# not Tested 
+class AssignDoctorToClinicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssignDoctorToClinic
+        fields = ['doctor','clinic','is_active']
+        read_only_fields = ['created_at']
+    def validate(self, attrs):
+        doctor = self.context.get('request').data.get('doctor')
+        clinic = self.context.get('request').data.get('doctor')
+        if AssignDoctorToClinic.objects.filter(doctor = doctor,clinic=clinic).exists():
+            raise ValidationError("this doctor is allready assigned to this clinic")
+        return attrs
+    
